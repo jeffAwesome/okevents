5354155b66cd0b7a498e97184b55e0e668cad1bb
Jeffrey Richardson <jeffreywebdevelopment@gmail.com>
2013-10-18 01:37:49 -0500

    Update event models spec test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
+include Capybara::DSL
+
+
+def before
+  @event = Event.create(title: 'Flaming Lips', location: 'Okc', date: '2015/10/15')
+end
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a month method that translates numbers to text' do
+    before
+    assert_equal('Oct', @event.event_month(10))
+  end
+
+  it 'has a time method that formats time' do
+    before
+    the_time = "19:45:00".to_time
+    assert_equal('07:45PM', @event.event_time(the_time))
+  end
+
 end
