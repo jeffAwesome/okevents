4051c2978c2aeaa3bd9858672869129f6cfd560c
Jeffrey Richardson <jeffreywebdevelopment@gmail.com>
2013-10-18 00:43:57 -0500

    Update logout method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Okevents::Application.routes.draw do
-  devise_for :users
+  devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' end
   root :to => 'events#index'
   resources :events
 end
